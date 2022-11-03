1b4ff95a5e7a2fea37dd215bfa1ade9917404b81
Shmuel Tzishinsky <a0527188257@gmail.com>
2022-11-04 01:59:54 +0200

    try test

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -6,10 +6,10 @@ import Puzzle from "./Puzzle";
 function index() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/menuMatch/*" element={<MenuMatch />} />
-      <Route path="/puzzle/*" element={<Puzzle />} />
-      <Route path="/quiz" element={<h1>Hi quiz</h1>} />
+      <Route path="/games-development/" element={<Home />} />
+      <Route path="/games-development/menuMatch/*" element={<MenuMatch />} />
+      <Route path="/games-development/puzzle/*" element={<Puzzle />} />
+      <Route path="/games-development/quiz" element={<h1>Hi quiz</h1>} />
       <Route path="*" element={<h1>Not found</h1>} />
     </Routes>
   );
