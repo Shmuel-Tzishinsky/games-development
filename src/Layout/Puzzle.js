6fac7d3b06f8f786bc5daa07954c880b9e3aff07
Shmuel Tzishinsky <a0527188257@gmail.com>
2023-04-02 15:31:49 +0300

    test

--- src/Layout/Puzzle.js
+++ src/Layout/Puzzle.js
@@ -1,16 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import SelectImg from "../pages/SelectPuzzle/SelectImg";
-import PuzzleGame from "../pages/SelectPuzzle/Puzzle";
 
 function Puzzle() {
-  return (
-    <Routes>
-      <Route index element={<SelectImg />} />
-      <Route path=":imgName" element={<SelectImg />} />
-      <Route path=":imgName/:level" element={<PuzzleGame />} />
-      <Route path=":imgName/:level/:nextLevel" element={<PuzzleGame />} />
-    </Routes>
-  );
+  return <Routes></Routes>;
 }
 
 export default Puzzle;
--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,20 +1,26 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "../pages/home/Home";
 import MenuMatch from "./MenuMatch";
-import Puzzle from "./Puzzle";
 import Quiz from "../pages/WritingPractice/Quiz";
 import GuessPicture from "../pages/triviaLetters/Quiz";
+import SelectImg from "../pages/SelectPuzzle/SelectImg";
+import PuzzleGame from "../pages/SelectPuzzle/Puzzle";
 
 function index() {
   return (
-    <Routes>
-      <Route path="/games-development/" element={<Home />} />
-      <Route path="/games-development/menuMatch/*" element={<MenuMatch />} />
-      <Route path="/games-development/puzzle/*" element={<Puzzle />} />
-      <Route path="/games-development/quiz" element={<Quiz />} />
-      <Route path="/games-development/guess-the-picture" element={<GuessPicture />} />
-      <Route path="*" element={<h1>Not found</h1>} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/games-development/" element={<Home />} />
+        <Route path="/games-development/menuMatch/*" element={<MenuMatch />} />
+        <Route path="/games-development/puzzle/" element={<SelectImg />} />
+        <Route path="/games-development/puzzle/:imgName" element={<SelectImg />} />
+        <Route path="/games-development/puzzle/:imgName/:level" element={<PuzzleGame />} />
+        <Route path="/games-development/puzzle/:imgName/:level/:nextLevel" element={<PuzzleGame />} />
+        <Route path="/games-development/quiz" element={<Quiz />} />
+        <Route path="/games-development/guess-the-picture" element={<GuessPicture />} />
+        <Route path="*" element={<h1>Not found</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
